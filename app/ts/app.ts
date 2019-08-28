eb3f4b067fbcbe65e379cc2b10b419cd50169a13
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-28 16:06:02 -0300

    Capturando dados no formulário

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,6 +1,4 @@
-// const date = new Date();
-// const negociacao = new Negociacao(date, 1, 100);
-// negociacao._quantidade = 3;
-// console.log(negociacao.quantidade);
-
-alert('oi')
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submit',controller.adiciona.bind(controller));
