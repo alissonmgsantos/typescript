f02d128f41d313814fdbbef4d1a5ab9a2d0d0635
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-28 15:46:24 -0300

    Configurando compilador para funcionar em tempo real

---
+++ app/ts/app.ts
@@ -0,0 +1,6 @@
+// const date = new Date();
+// const negociacao = new Negociacao(date, 1, 100);
+// negociacao._quantidade = 3;
+// console.log(negociacao.quantidade);
+
+alert('oi')
--- app/ts/controllers/Negociacao.ts
+++
@@ -1,5 +0,0 @@
-class Negociacao {
-    construtor(){
-        
-    }
-}
