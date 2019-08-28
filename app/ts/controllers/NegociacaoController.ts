e408f1623c86fd42c9f6576fd42efe0529e34ee2
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-28 16:29:59 -0300

    Criação da classe negociações que vai encapsular a lista de negociações

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes();
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
@@ -18,8 +19,15 @@ class NegociacaoController {
             new Date(this._inputData.value.replace(/-/g, ',')),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
-        )
+        );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        });
+
     }
 }
